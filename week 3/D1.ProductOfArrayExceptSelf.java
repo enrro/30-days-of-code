699f50355ab9ff2631ff73a3be9d95820e13ece4
Edgar Alfredo Briceño Gonzalez <enrro@Edgars-MacBook-Pro.local>
2020-08-22 21:40:20 -0500

    code step by step comments

--- week 3/D1.ProductOfArrayExceptSelf.java
+++ week 3/D1.ProductOfArrayExceptSelf.java
@@ -23,12 +23,15 @@ class Solution {
         for(int i = 1; i < nums.length; i++){
            L[i] = L[i - 1] * nums[i - 1]; 
         }
+        //[1,2,3,4]
+        //L = [1, 1, 2, 6]
         System.out.println(Arrays.toString(L));
         
         R[nums.length-1] = 1;
         for(int i = nums.length - 2; i >= 0; i--){
             R[i] = R[i + 1] * nums[i + 1];
         }
+        //R = [24,12,4,1]
         System.out.println(Arrays.toString(R));
         for(int i = 0; i < nums.length; i++){
             numRes[i] = L[i] * R[i];
