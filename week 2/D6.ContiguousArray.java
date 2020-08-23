4d5ba4652e236035fdc86f4212f7ef4480469f74
Edgar Alfredo Briceño Gonzalez <enrro@Edgars-MacBook-Pro.local>
2020-08-22 21:20:14 -0500

    code explanation

--- week 2/D6.ContiguousArray.java
+++ week 2/D6.ContiguousArray.java
@@ -11,12 +11,19 @@ Output: 2
 Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
 Note: The length of the given binary array will not exceed 50,000.
 */
+/*
+the map is a relationship between the number of ocurrences and the position it appears. 
+when we find the number of ocurrences we measure the distance and compare with the current max
+if the current max is larger then that means that we have a new max.
+*/
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
     public int findMaxLength(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0,-1);
         int maxlen = 0, count = 0;
-        
         for (int i = 0;i < nums.length; i++){
             count = count + (nums[i] == 1 ? 1 : -1);
             if(map.containsKey(count)){
